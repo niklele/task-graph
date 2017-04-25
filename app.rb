54f9cbace47185596367d66d3126eb8d8d168423
Nikhil Lele <nik.lele@gmail.com>
2017-04-24 21:29:53 -0700

    minor fix

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'graphviz'
 
 get '/' do
-  "Hello World!"
+    # Create a new graph
+    g = GraphViz.new( :G, :type => :digraph )
+
+    # Create two nodes
+    hello = g.add_nodes( "Hello" )
+    world = g.add_nodes( "World" )
+
+    # Create an edge between the two nodes
+    g.add_edges( hello, world )
+
+    # Generate output image
+    # g.output( :png => "hello_world.png" )
+    return g.output( :none => String)
+
 end
